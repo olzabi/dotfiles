3cb1f5e12f3a4410bb175efaa60d826fdff29679
Oleg Zabiiaka <olzabi.dev@gmail.com>
2026-09-20 22:41:56 +0000

    wip: yazi upgrade, scripts, glow(tui for md), hunk

--- editor/yazi/init.lua
+++ editor/yazi/init.lua
@@ -16,12 +16,6 @@ require("projects"):setup({
 	},
 })
 
-th.git = th.git or {}
-th.git.modified_sign = "M"
-th.git.deleted_sign = "D"
-th.git.untracked_sign = "U"
-th.git.ignored_sign = "I"
-
 require("git"):setup({
 	order = 500,
 })
